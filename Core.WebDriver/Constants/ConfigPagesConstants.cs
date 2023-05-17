cf31524a19ac6882058519c6a2884ec8adfc6cb9
igorlviv2018 <39830016+igorlviv2018@users.noreply.github.com>
2023-05-17 18:14:19 +0300

    Delete Core.WebDriver/Constants directory

--- Core.WebDriver/Constants/ConfigPagesConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Taf.UI.Core.Constants
-{
-    public class ConfigPagesConstants
-    {
-        public const string GeneralSettings  = "General settings";
-
-        public const string DashboardSettingsLink  = "/admin/config/appearance/dashboard-settings";
-    }
-}
